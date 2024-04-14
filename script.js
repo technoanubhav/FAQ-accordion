c99033bc4ce9d6a7aa40cf8be5d7fe01eff41d9d
priyansh70 <priyansh70890@gmail.com>
2024-04-14 19:01:23 +0530

    Add styling logic in the JS and more optimize

--- script.js
+++ script.js
@@ -32,7 +32,15 @@ let idx = null;
 
 // Optimize Code 
 
-function toggleAccordian(image, answer, index) {
+function removeStyle(question) {
+    question.parentElement.style.border = 'transparent';
+    question.style.color = 'black';
+}
+
+function toggleAccordian(question, index) {
+
+    const image = question.querySelector('.icon');
+    const answer = question.nextElementSibling;
 
     // If already Accordian open -- so close previous one
     if (idx !== null && idx !== index) {
@@ -42,28 +50,30 @@ function toggleAccordian(image, answer, index) {
 
         previousAnswer.classList.remove('active');
         previousImage.src = './assets/images/icon-plus.svg';
+        removeStyle(previousQuestion, previousQuestion);
     }
-    
+
     // // When Click on the Open Accordian
     if (idx === index) {
         image.src = './assets/images/icon-plus.svg';
         answer.classList.remove('active');
+        removeStyle(question);
         idx = null;
     }
     else {
         // When it is first time click on Accordian
         image.src = './assets/images/icon-minus.svg';
         answer.classList.add('active');
+        question.parentElement.style.border = '2px solid #AD28EB';
+        question.style.color = '#AD28EB';
         idx = index;
     }
 
 }
 
 questions.forEach((question, index) => {
-    const image = question.querySelector('.icon');
-    const answer = question.nextElementSibling;
 
     question.addEventListener('click', () => {
-        toggleAccordian(image, answer, index);
+        toggleAccordian(question, index);
     })
 })
